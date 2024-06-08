0ebb437fc933a2f3fa89d0e73ce9f9d975f14ec5
IsmoilovMax <maxmudjon2121@gmail.com>
2024-06-08 22:33:21 +0900

    feat: member service model integraion

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response}  from "express";
 import {T} from "../libs/types/common";
+import MemberService from "../models/Member.service";
 
 const restaurantController: T = {};
 
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,8 @@ import app from "./app";
 // Подключение к MongoDB с использованием переменной среды MONGO_URL
 mongoose.set('strictQuery', false);
 const mongoUrl = process.env.MONGO_URL;
-mongoose.connect(mongoUrl as string, {})
+mongoose
+    .connect(mongoUrl as string, {})
     .then(data => {
         console.log("MONGO-DB connectin succeed!");
 

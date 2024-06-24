862d709956fec06098b5c11bc080b2e33f742ca1
IsmoilovMax <maxmudjon2121@gmail.com>
2024-06-24 20:07:06 +0900

    ADD TACK -M

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,18 @@
-// L-TASK: 
+// M-TASK: 
 
-// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
-// MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
 
-
-function reverseString(parametr: string): string {
-    return parametr.split('').reverse().join('');
-}
-
-console.log(reverseString("I like reading documentation")); // Натижа: "noitatnemucod gnidaer ekil "
+function getSquareNumbers(numbers: number[]): { number: number; square: number }[] {
+    return numbers.reduce((acc: { number: number; square: number }[], number) => {
+      acc.push({ number, square: number * number });
+      return acc;
+    }, []);
+  }
   
+  const result = getSquareNumbers([5, 3, 9]);
+  console.log(result);
+
 /* project Standarts:
     -Logging standarts
     -Naming standards

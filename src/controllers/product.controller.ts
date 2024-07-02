5f844cc0249fe47957747d330b0eb435af8599e8
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-02 10:38:34 +0900

    fix: modify products page based on product data

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -21,7 +21,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
         console.log("getAllProducts");
         const data = await productService.getAllProducts();
         console.log("data:", data)
-        res.render("products", { productData: data });
+        res.render("products", { products: data });
         
     } catch (err) {
         console.log("Error, getAllProducts:", err);

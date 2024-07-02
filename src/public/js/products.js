25e52bb94e5b280bea077a705565f919cae5d38c
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-02 14:10:29 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -22,6 +22,26 @@ $(function () {
         $(".dish-container").slideToggle(100);
         $("#process-btn").css("display", "flex");
     });
+
+    $(".new-product-status").on("change", async function(e) {
+        const id = e.target.id;
+        const productStatus = $(`#${id}.new-product-status`).val();
+        console.log("id:", id);
+        console.log("productStatus:", productStatus);
+
+        try {
+            const response = await axios.post(`/admin/product/${id}`, {productStatus: productStatus});
+            console.log("response:", response);
+            const result = response.data;
+            if (result.data) {
+                console.log("Product updated!");
+                $(".new-product-status").blur();
+            }else  alert("Product update failed!")
+        }catch(err) {
+            console.log(err);
+            alert("Product update failed!")
+        }
+    });
 });
 
 

33fa67c0521ecd5b988291d2c5c204c23689c43d
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-12 00:36:36 +0900

    T-TACK

--- src/train.ts
+++ src/train.ts
@@ -1,33 +1,16 @@
-// S-TASK:
+// T-TASK:
 
-// Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
-// MASALAN: missingNumber([3, 0, 1]) return 2
+// Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
-// Функция для нахождения пропущенного числа в массиве
-function missingNumber(nums: number[]): number {
-    // n это длина массива
-    const n = nums.length;
-    
-    // Вычисляем сумму первых n натуральных чисел, включая 0
-    const totalSum = (n * (n + 1)) / 2;
-    
-    // Вычисляем сумму элементов данного массива
-    const arraySum = nums.reduce((acc, num) => {
-        console.log(`acc: ${acc}, num: ${num}`); // Вывод текущего значения аккумулятора и элемента массива
-        return acc + num; // Добавляем текущий элемент к аккумулятору
-    }, 0); // Начальное значение аккумулятора равно 0
 
-    console.log(`totalSum: ${totalSum}, arraySum: ${arraySum}`); // Вывод общей суммы и суммы массива
-    
-    // Разница между общей суммой и суммой массива - это пропущенное число
-    return totalSum - arraySum;
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+    return arr1.concat(arr2).sort((a, b) => a - b);
 }
 
-// Примеры использования:
-console.log(missingNumber([3, 0, 1])); // Результат: 2
-console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // Результат: 8
-console.log(missingNumber([0, 1])); // Результат: 2
-console.log(missingNumber([0])); // Результат: 1
+
+const result = mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
+console.log(result); 
 
 
 

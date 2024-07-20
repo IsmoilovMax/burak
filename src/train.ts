b4093a7f9c7bf4c3f782d692ccbed9f2934ec76d
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-20 21:26:41 +0900

    X-TASK:

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,32 @@
-// W-TASK:
+// X-TASK:
 
-// Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
-// MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
+//  Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri 
+//object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
+//  MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
 
-function chunkArrayReduce<T>(array: T[], size: number): T[][] {
-  return array.reduce((acc: T[][], current: T, i: number) => {
-    if (i % size === 0) acc.push(array.slice(i, i + size));
-    return acc;
-  }, []);
+function countReduce(obj: any, searchString: string): number {
+  let count = 0;
+
+  function search(obj: any): void {
+    if (obj !== null && typeof obj === 'object') {
+      Object.keys(obj).reduce((acc, key) => {
+        if (key === searchString || obj[key] === searchString) {
+          count++;
+        }
+        search(obj[key]);
+        return acc;
+      }, {});
+    }
+  }
+
+  search(obj);
+  return count;
 }
-// Big O: O(n)
 
-chunkArrayReduce([1,2,3,4,5,6,7,8,9,10], 3)
-console.log(chunkArrayReduce([1,2,3,4,5,6,7,8,9,10], 3))
+// Test
+console.log(countReduce({ model: 'Bugatti', steer: { model: 'HANKOOK', size: 30, brand: 'model' } }, 'model')); 
+
+
 /*
 ---Validations---
 frontend

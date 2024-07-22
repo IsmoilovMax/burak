0f163c4b8fcc087e82419210649ebc32f84783d0
IsmoilovMax <maxmudjon2121@gmail.com>
2024-07-22 17:54:58 +0900

    feat: Y-TASK : reduce 방법으로 해결했다

--- src/train.ts
+++ src/train.ts
@@ -1,30 +1,18 @@
-// X-TASK:
+// Y-TASK:
 
-//  Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri 
-//object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
-//  MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
+//  Shunday function yozing, uni 2 ta array parapetri bolsin. Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda qaytarsin
+//  MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3]
 
-function countReduce(obj: any, searchString: string): number {
-  let count = 0;
 
-  function search(obj: any): void {
-    if (obj !== null && typeof obj === 'object') {
-      Object.keys(obj).reduce((acc, key) => {
-        if (key === searchString || obj[key] === searchString) {
-          count++;
-        }
-        search(obj[key]);
-        return acc;
-      }, {});
-    }
-  }
-
-  search(obj);
-  return count;
+function findIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.reduce((acc, value) => {
+      if (arr2.includes(value) && !acc.includes(value)) {
+          acc.push(value);
+      }
+      return acc;
+  }, [] as number[]);
 }
-
-// Test
-console.log(countReduce({ model: 'Bugatti', steer: { model: 'HANKOOK', size: 30, brand: 'model' } }, 'model')); 
+console.log(findIntersection([1, 2, 5, 9], [3, 2, 9, 1])); // [ 1, 2, 9 ]
 
 
 /*
